010e57386b352bff4a12fee97c65dc91a2389fde
euiyang <euiyang2000@gmail.com>
2023-03-26 21:48:29 +0900

    remove etc

--- Front_end/areum/src/App.js
+++ Front_end/areum/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
 import './App.css';
+import Profile from './pages/profile/Profile.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/profile" element={<Profile/>}/>
+        </Routes>
+        <Link to="/profile">pro </Link>
+        <Link to="/">home </Link>
+      </BrowserRouter>
+      
     </div>
   );
 }
