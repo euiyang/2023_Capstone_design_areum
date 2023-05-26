ea0f637e88514c83cd5b68a2eb0bad0bb17c651b
euiyang <euiyang2000@gmail.com>
2023-05-26 23:29:49 +0900

    jwt complete

---
+++ Back_end/areum/src/main/java/com/example/demo/config/SecurityConfiguration.java
@@ -0,0 +1,12 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurer {
+    @Bean
+
+}
