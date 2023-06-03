67d5821b75e6c48f64d8c2f88f4dc18f34155693
minoii311 <95284200+minoii311@users.noreply.github.com>
2023-06-03 11:49:45 +0900

    .

--- Front_end/areum/src/pages/home/Home2.js
+++ Front_end/areum/src/pages/home/Home2.js
@@ -58,11 +58,11 @@ function Home2(){
         <div className='home'>
             <div className="header">
             <Link to="/"><img className="logo" alt="logo" src="img/areumlogo.png" /></Link>
-                <ui className="header-menu">
-                 <li><Link to="/Lab" style={{ textDecoration: "none" }}> 연구실 </Link></li>
-                 <li><Link to="/Group" style={{ textDecoration: "none" }}> 동아리 </Link></li>
-                 <li><Link to="/Study" style={{ textDecoration: "none" }}> 스터디 </Link></li>
-                </ui>
+        <ui className="header-menu">
+         <li><Link to="/Lab" className="menu-item"> 연구실 </Link></li>
+         <li><Link to="/Group" className="menu-item"> 동아리 </Link></li>
+         <li><Link to="/Study" className="menu-item"> 스터디 </Link></li>
+        </ui>
         <div className="header-buttons">
             <Link to ="/signIn">
             <button className="signin-btn">로그인</button>
